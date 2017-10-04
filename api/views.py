39f8c512834b3aa72d489ba6168128eab4651f03
darwinv <darwin.vasz@gmail.com>
2017-10-04 10:08:01 -0500

    cambios modificados para funcionamiento del fixture

--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from rest_framework.generics import ListCreateAPIView, UpdateAPIView
 from api.models import User, Client, Category, Specialist
 from api.serializers import ClientSerializer
 from rest_framework.response import Response
-from rest_framework import status, permissions, viewsets import django_filters
+from rest_framework import status, permissions, viewsets
+import django_filters.rest_framework
 from api.serializers import UserSerializer, CategorySerializer, SpecialistSerializer
 from django.http import Http404
 from rest_framework.pagination import PageNumberPagination
