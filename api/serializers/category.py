cba72ed997c1670e274553da62b0018efec9305e
darwinv <darwin.vasz@gmail.com>
2017-11-20 12:13:33 -0500

    devolver traducida la data

--- api/serializers/category.py
+++ api/serializers/category.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from api.models import Category
 from django.utils.translation import ugettext_lazy as _
 class CategorySerializer(serializers.ModelSerializer):
-    # name = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
     class Meta:
         model = Category
         fields = ('id', 'name', 'image', 'description')
 
-    # def get_name(self, obj):
-    #     return _(obj.name)
+    def get_name(self, obj):
+        return _(obj.name)
