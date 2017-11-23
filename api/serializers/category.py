5cae632a3bd3cc5a3332df132e4d8e542660e9a8
darwinv <darwin.vasz@gmail.com>
2017-11-23 14:49:43 -0500

    comments and pep8

--- api/serializers/category.py
+++ api/serializers/category.py
@@ -1,11 +1,20 @@
+"""Especialidades."""
 from rest_framework import serializers
 from api.models import Category
 from django.utils.translation import ugettext_lazy as _
+
+
 class CategorySerializer(serializers.ModelSerializer):
+    """Serializer de Especialidades."""
+
     name = serializers.SerializerMethodField()
+
     class Meta:
+        """Modelo Category y sus campos."""
+
         model = Category
         fields = ('id', 'name', 'image', 'description')
 
     def get_name(self, obj):
+        """Devuelve el nombre de la especialidad."""
         return _(obj.name)
