8e4d4dec653abc8e0c17acebc2c67e858bb1b640
darwinv <darwin.vasz@gmail.com>
2018-02-21 10:51:22 -0500

    Se integro Firebase Cloud Messaging en la api

---
+++ fcm/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class FcmConfig(AppConfig):
+    """Configuration of app."""
+
+    name = 'fcm'
+    FCM_SETTINGS = {
+     "FCM_SERVER_KEY": "AIzaSyBFp1b8qmoSn13YFbpfTf3cIVgmjd_SCrY",
+     "FCM_SERVER": "https://fcm.googleapis.com/fcm/send",
+    }
