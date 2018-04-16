2bcf79b09d1bdd8fc11829f9f5db18851004a250
darwinv <darwin.vasz@gmail.com>
2018-04-16 18:29:09 -0500

    acomode las urls siguiendo pep8

--- api/urls.py
+++ api/urls.py
@@ -13,42 +13,64 @@ urlpatterns = [
     # Clientes
     url(r'^clients/$', actors.ClientListView.as_view(), name='clients'),
     # Servicio para logueo de clientes
-    url(r'^clients-users/(?P<username>[^@]+@[^@]+\.[^@]+)/$', actors.ClientDetailByUsername.as_view(),
+    url(r'^clients-users/(?P<username>[^@]+@[^@]+\.[^@]+)/$',
+        actors.ClientDetailByUsername.as_view(),
         name='client-detail-username'),
-    #todos los planes activos de un cliente
-    url(r'^clients/plans/$', plan.ClientPlansView.as_view(), name='client-plans'),
+    # todos los planes activos de un cliente
+    url(r'^clients/plans/$', plan.ClientPlansView.as_view(),
+        name='client-plans'),
 
-    url(r'^specialists-users/(?P<username>[^@]+@[^@]+\.[^@]+)/$', actors.SpecialistDetailByUsername.as_view(),
+    url(r'^specialists-users/(?P<username>[^@]+@[^@]+\.[^@]+)/$',
+        actors.SpecialistDetailByUsername.as_view(),
         name='specialist-detail-username'),
 
-    url(r'^sellers-users/(?P<username>[^@]+@[^@]+\.[^@]+)/$', actors.SellerDetailByUsername.as_view(),
+    url(r'^sellers-users/(?P<username>[^@]+@[^@]+\.[^@]+)/$',
+        actors.SellerDetailByUsername.as_view(),
         name='seller-detail-username'),
     # detalle de cliente
-    url(r'^clients/(?P<pk>[0-9]+)/$', actors.ClientDetailView.as_view(), name='client-detail'),
+    url(r'^clients/(?P<pk>[0-9]+)/$', actors.ClientDetailView.as_view(),
+        name='client-detail'),
     # Especialidades
-    url(r'^categories/$', category.CategoryListView.as_view(), name='categories'),
-    url(r'^categories/(?P<pk>[0-9]+)/$', category.CategoryDetailView.as_view(), name='category-detail'),
+    url(r'^categories/$', category.CategoryListView.as_view(),
+        name='categories'),
+    url(r'^categories/(?P<pk>[0-9]+)/$',
+        category.CategoryDetailView.as_view(), name='category-detail'),
     # Especialistas
-    url(r'^specialists/$', actors.SpecialistListView.as_view(), name='specialists'),
+    url(r'^specialists/$', actors.SpecialistListView.as_view(),
+        name='specialists'),
     # Especialistas mensajes
-    url(r'^specialists/list-messages/$', actors.SpecialistMessagesListView.as_view(), name='specialists-list-messages'),
+    url(r'^specialists/list-messages/$',
+        actors.SpecialistMessagesListView.as_view(),
+        name='specialists-list-messages'),
 
-    url(r'^specialists/(?P<pk>[0-9]+)/$', actors.SpecialistDetailView.as_view(), name='specialist-detail'),
-    #numero de consultas mensual y anual de un especialista
-    url(r'^specialists/query-count/$', actors.SpecialistQueryCountView.as_view(), name='specialist-query-count'),
+    url(r'^specialists/(?P<pk>[0-9]+)/$',
+        actors.SpecialistDetailView.as_view(),
+        name='specialist-detail'),
+    # numero de consultas mensual y anual de un especialista
+    url(r'^specialists/query-count/$',
+        actors.SpecialistQueryCountView.as_view(),
+        name='specialist-query-count'),
 
     url(r'^account_status/specialists/(?P<pk>[0-9]+)/$',
-        actors.SpecialistAccountView.as_view(), name='specialist-account-status'),
+        actors.SpecialistAccountView.as_view(),
+        name='specialist-account-status'),
     # Consultas
     # Listado de Consultas por especialista
-    # url(r'^specialist-queries/$', query.QueryListSpecialistView.as_view(), name='queries-specialist'),
+    # url(r'^specialist-queries/$', query.QueryListSpecialistView.as_view(),
+    # name='queries-specialist'),
 
     # Listado de Consulta y Creación de consultas por cliente
-    url(r'^client/queries/$', query.QueryListClientView.as_view(), name='queries-client'),
+    url(r'^client/queries/$',
+        query.QueryListClientView.as_view(),
+        name='queries-client'),
     # Queries de cliente por categoria
-    url(r'^queries/categories/(?P<pk>[0-9]+)/$', query.QueryChatClientView.as_view(), name='query-chat-client'),
+    url(r'^queries/categories/(?P<pk>[0-9]+)/$',
+        query.QueryChatClientView.as_view(),
+        name='query-chat-client'),
     # Consultas de especialista por cliente
-    url(r'^queries/clients/(?P<pk>[0-9]+)/$', query.QueryChatSpecialistView.as_view(), name='query-chat-specialist'),
+    url(r'^queries/clients/(?P<pk>[0-9]+)/$',
+        query.QueryChatSpecialistView.as_view(),
+        name='query-chat-specialist'),
 
     url(r'^queries/(?P<pk>[0-9]+)/$', query.QueryDetailView.as_view(), name='query-detail'),
     url(r'^query-last/(?P<category>[0-9]+)/$', query.QueryLastView.as_view(), name='last-query-bycategory'),
