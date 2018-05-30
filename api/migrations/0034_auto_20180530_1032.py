10d6e97205c1fc43e916720a45c0b99ffd49f49e
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-05-30 10:42:01 -0500

    .

--- api/migrations/0034_auto_20180530_1032.py
+++ api/migrations/0034_auto_20180530_1032.py
@@ -15,27 +15,27 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='client',
             name='ocupation',
-            field=models.PositiveIntegerField(choices=[(1, 'Employer'), (2, 'Independent worker'), (3, 'Employee'), (4, 'Worker'), (5, 'Worker in a family business'), (6, 'Home worker'), (7, 'Other')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Employer'), (2, 'Independent worker'), (3, 'Employee'), (4, 'Worker'), (5, 'Worker in a family business'), (6, 'Home worker'), (7, 'Other')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='matchacquired',
             name='status',
-            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Declined')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Declined')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='matchacquiredfiles',
             name='type_file',
-            field=models.PositiveIntegerField(choices=[(2, 'Image'), (3, 'Voice'), (4, 'Document')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(2, 'Image'), (3, 'Voice'), (4, 'Document')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='matchacquiredlog',
             name='status',
-            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Declined')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Declined')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='message',
             name='content_type',
-            field=models.PositiveIntegerField(choices=[(1, 'Text'), (2, 'Image'), (3, 'Video'), (4, 'Voice'), (5, 'Document')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Text'), (2, 'Image'), (3, 'Video'), (4, 'Voice'), (5, 'Document')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='monthlyfee',
@@ -45,36 +45,36 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='payment',
             name='status',
-            field=models.PositiveIntegerField(choices=[(1, 'Pending'), (2, 'Accepted'), (3, 'Declined')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Pending'), (2, 'Accepted'), (3, 'Declined')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='query',
             name='status',
-            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Answered'), (4, 'To score'), (5, 'Absolved')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Answered'), (4, 'To score'), (5, 'Absolved')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='querylogs',
             name='status_log',
-            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Answered'), (4, 'To score'), (5, 'Absolved')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Requested'), (2, 'Accepted'), (3, 'Answered'), (4, 'To score'), (5, 'Absolved')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='sellercontactnoefective',
             name='document_type',
-            field=models.PositiveIntegerField(choices=[(1, 'DNI'), (2, 'Passport'), (3, 'Foreign Card')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'DNI'), (2, 'Passport'), (3, 'Foreign Card')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='sellercontactnoefective',
             name='ocupation',
-            field=models.PositiveIntegerField(choices=[(1, 'Employer'), (2, 'Independent worker'), (3, 'Employee'), (4, 'Worker'), (5, 'Worker in a family business'), (6, 'Home worker'), (7, 'Other')], max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Employer'), (2, 'Independent worker'), (3, 'Employee'), (4, 'Worker'), (5, 'Worker in a family business'), (6, 'Home worker'), (7, 'Other')], max_length=1, null=True),
         ),
         migrations.AlterField(
             model_name='user',
             name='document_type',
-            field=models.PositiveIntegerField(choices=[(1, 'DNI'), (2, 'Passport'), (3, 'Foreign Card')], max_length=1, verbose_name='type document', blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'DNI'), (2, 'Passport'), (3, 'Foreign Card')], max_length=1, verbose_name='type document', null=True),
         ),
         migrations.AlterField(
             model_name='user',
             name='status',
-            field=models.PositiveIntegerField(choices=[(1, 'Pending'), (2, 'Activate'), (3, 'Reject'), (4, 'Deactivated')], default='1', max_length=1, blank=True),
+            field=models.PositiveIntegerField(choices=[(1, 'Pending'), (2, 'Activate'), (3, 'Reject'), (4, 'Deactivated')], default='1', max_length=1, null=True),
         ),
     ]
