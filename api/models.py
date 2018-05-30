4d40e0316c1376d922c8fa3899259066b98f2945
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-05-30 11:18:04 -0500

    auto merge choices

--- api/models.py
+++ api/models.py
@@ -218,7 +218,7 @@ class SellerContactNoEfective(models.Model):
     about = models.CharField(max_length=255, null=True, blank=True)
     cellphone = models.CharField(max_length=14, blank=True, null=True)
     telephone = models.CharField(max_length=14, blank=True, null=True)
-    ocupation = models.PositiveIntegerField(max_length=1, choices=Ch.client_ocupation, blank=True)
+    ocupation = models.PositiveIntegerField(max_length=1, choices=Ch.client_ocupation, null=True)
     profession = models.CharField(max_length=45, null=True)
     business_name = models.CharField(max_length=45, null=True)
     commercial_reason = models.CharField(max_length=45, null=True)
