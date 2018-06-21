3461f04ed3c2c9016cb303c5b740afe7abffb6cf
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-06-21 13:04:02 -0500

    file update

--- api/views/query.py
+++ api/views/query.py
@@ -454,7 +454,8 @@ class QueryUploadFilesView(APIView):
         thumb = resize_img(file, 256)
         if thumb:
             name_file_thumb, extension_thumb = os.path.splitext(thumb.name)
-            if extension_thumb != 'mp4':
+            
+            if extension != '.mp4':
                 url_thumb = s3_upload_file(thumb, filename + '-thumb' + extension_thumb)
                 remove_file(thumb)
             else:
