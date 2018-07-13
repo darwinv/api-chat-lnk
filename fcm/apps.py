c37cc9cf6e204e0af1dd9fcf562726b43a89ce3f
darwinv <darwin.vasqz@gmail.com>
2018-07-13 10:17:23 -0500

    agregado

--- fcm/apps.py
+++ fcm/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class FcmConfig(AppConfig):
     """Configuration of app."""
-
+# cargarconfiguracion del settings secret
     name = 'fcm'
     FCM_SETTINGS = {
      "FCM_SERVER_KEY": "AIzaSyBFp1b8qmoSn13YFbpfTf3cIVgmjd_SCrY",
