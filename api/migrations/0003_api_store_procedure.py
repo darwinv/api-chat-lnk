bc4a762bf18c3475d53ab9d7f3761eae32d6f300
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-07-13 11:25:56 -0500

    arreglando error test obj declinar

--- api/migrations/0003_api_store_procedure.py
+++ api/migrations/0003_api_store_procedure.py
@@ -7,7 +7,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('api', '0023_auto_20180313_0944'),
+        ('api', '0002_auto_20180713_0849'),
     ]
 
     operations = [
