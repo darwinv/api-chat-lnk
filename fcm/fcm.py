ee1666a89358bf8da38ae804166c7d416bc05f10
darwinv <darwin.vasqz@gmail.com>
2018-07-19 11:48:41 -0500

    hardcode

--- fcm/fcm.py
+++ fcm/fcm.py
@@ -10,7 +10,7 @@ class Notification:
         """Funcion para enviar mensaje a traves de topicos en Firebase."""
         api_key = FcmConfig.FCM_SETTINGS.get("FCM_SERVER_KEY")
         push = FCMNotification(api_key=api_key)
-        result = push.notify_topic_subscribers(topic_name=user_id, data_message=data)
+        result = push.notify_topic_subscribers(topic_name="/topics/user-12", data_message=data)
         # do not raise errors, pyfcm will raise exceptions if response status will
         # be anything but 200
         return result
