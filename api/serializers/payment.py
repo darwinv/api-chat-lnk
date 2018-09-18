470a55aab33ee7bfaf951951abffd5462ff21a83
darwinv <darwin.vasqz@gmail.com>
2018-09-18 10:50:29 -0500

    pagos  se acumulan las  consultas disponibles

--- api/serializers/payment.py
+++ api/serializers/payment.py
@@ -74,7 +74,7 @@ class PaymentSerializer(serializers.ModelSerializer):
             # debo chequear si es por cuotas o no
             if fee.sale.is_fee:
                 # libero el numero de consultas que corresponde
-                qacd.available_queries = qacd.query_plans.query_quantity / qacd.query_plans.validity_months
+                qacd.available_queries = qacd.available_queries + (qacd.query_plans.query_quantity / qacd.query_plans.validity_months)
                 # actualizo cuantas consultas faltan por pagar
                 qacd.queries_to_pay = qacd.query_plans.query_quantity - qacd.available_queries
             else:
