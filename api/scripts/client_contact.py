fbf25bfc53bf8837f89890b48fae44eaac680015
Walter Capa <walterc316@gmail.com>
2018-10-26 18:38:52 -0500

    Se corrigio el script para tomar en cuenta todos los atributos, incluso aquellos que son null en client

--- api/scripts/client_contact.py
+++ api/scripts/client_contact.py
@@ -5,33 +5,20 @@ import pdb
 def get_data(client):
     """Devuelve data que servira para crear un contacto a partir de un cliente"""
 
-    type_contact = 3 if Sale.objects.filter(client=client.id, status__range=(2, 3)) else 1
-    data = {
-            'first_name': client.first_name,
-            'last_name': client.last_name,
-            'type_contact': type_contact,
-            'type_client': client.type_client,
-            'civil_state': client.civil_state,
-            'birthdate': client.birthdate,
-            "address": client.address,
-            'sex': client.sex,
-            'document_type': client.document_type,
-            'document_number': client.document_number,
-            'email_exact': client.email_exact,
-            'telephone': client.telephone,
-            'cellphone': client.cellphone,
-            'activity_description': client.activity_description,
-            'level_instruction': client.level_instruction,
-            'institute': client.institute,
-            'profession': client.profession,
-            'ocupation': client.ocupation,
-            "latitude": "-77.0282400",
-            "longitude": "-12.0431800",
-            'about': client.about,
-            'seller': client.seller_assigned,
-            'nationality': client.nationality,
-            'residence_country': client.residence_country
-        }
+    fields = ['first_name', 'last_name', 'type_client', 'civil_state', 'birthdate', "address", 'sex',
+            'document_type', 'document_number', 'email_exact', 'telephone', 'cellphone', 'activity_description',
+            'level_instruction', 'institute', 'profession', 'ocupation', "latitude", "longitude", 'about',
+            'nationality', 'residence_country', 'cciu', 'photo', 'code_telephone', 'code_cellphone', 'business_name',
+            'commercial_reason', 'agent_firstname', 'agent_lastname', 'position', 'ruc', 'economic_sector',
+            'foreign_address']
+
+    data = {}
+
+    for field in fields:
+        data[field] = getattr(client, field) if hasattr(client, field) else None
+
+    data['type_contact'] = 3 if Sale.objects.filter(client=client.id, status__range=(2, 3)) else 1
+    data['seller'] = client.seller_assigned
 
     return data
 
