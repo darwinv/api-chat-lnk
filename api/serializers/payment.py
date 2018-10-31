0f65194df88cd3b39092542a0aa4fb9a68e18925
darwinv <darwin.vasqz@gmail.com>
2018-10-31 11:56:58 -0500

    fixed fixture test match

--- api/serializers/payment.py
+++ api/serializers/payment.py
@@ -232,7 +232,6 @@ class PaymentMatchClientSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Crear pago de especialista."""
         match = validated_data.pop('match')
-
         instance = Payment(**validated_data)
         instance.save()
         match.status = 5
