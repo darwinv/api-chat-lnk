91a59ff5fb5c6a2178c457232ffa13e029876bd4
darwinv <darwin.vasqz@gmail.com>
2018-11-05 09:22:10 -0500

    cambio de nombre psicologia organizacional, por gestion de personas

--- api/tests/test_purchase.py
+++ api/tests/test_purchase.py
@@ -179,5 +179,4 @@ class PurchaseQueryPromotionalPlans(APITestCase):
         response2 = client.post(reverse('purchase'),
                                 data=json.dumps(self.valid_payload),
                                 content_type='application/json')
-
         self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
